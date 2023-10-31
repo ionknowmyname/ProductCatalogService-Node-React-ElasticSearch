cc473bdf8488470fee96adad0df5e8cb69ce56b1
Faithful Olaleru <faithfulolaleru09@gmail.com>
2023-10-31 18:41:13 +0100

    refactor code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors");
-const product = require("./routes/product");
+const product = require("../routes/product");
 
 const { connectDB } = require("./dbConnection")
 
